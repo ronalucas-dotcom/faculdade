a8a4af0db3fcb79708a512d2ed1f8eeff0f0726e
Antonio Lucas <ronalucas2007@gmail.com>
2025-09-27 12:55:26 -0300

    Atualiza exercício 05

--- lista 2/05.c
+++ lista 2/05.c
@@ -1,1 +1,37 @@
-#
+#include <stdio.h>
+int main(){
+     int n;
+    int maior, menor;
+    int primeiro = 1; 
+
+    while (1) {
+        printf("Digite um numero (0 para encerrar): ");
+        scanf("%d", &n);
+
+        if (n == 0) {
+            break; 
+        }
+
+        if (primeiro) {
+            maior = menor = n; 
+            primeiro = 0;
+        } else {
+            if (n > maior) {
+                maior = n;
+            }
+            if (n < menor) {
+                menor = n;
+            }
+        }
+    }
+
+    if (primeiro) {
+        printf("Nenhum numero foi digitado.\n");
+    } else {
+        printf("Maior numero: %d\n", maior);
+        printf("Menor numero: %d\n", menor);
+    }
+
+    return 0;
+}
+
